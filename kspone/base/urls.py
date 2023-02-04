99f56f56eafcbbba2a8206ed4b41348a9ac92fac
SarangSpin <sarangmani007@gmail.com>
2023-02-04 21:09:44 +0530

    updte srng

--- kspone/base/urls.py
+++ kspone/base/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns =[
+    
     path('login/login', views.loginpage, name="login"),
-    path('fingerprint',views.fingerprint,name="fingerprint"),
-    path('userguide',views.userguide,name="userguide"),
-    path('search',views.search,name="search"),
     path('help/', views.loginhelp, name="help"),
     path('', views.home, name="home"),
-    path("logout/", views.logoutuser, name = "logout")
-
+    path('search/', views.search, name="search"),
+    path('fingerprint/', views.fingerprint, name="fingerprint"),
+    path('userguide/', views.userguide, name="userguide"),
+    path("logout/", views.logoutuser, name = "logout"),
+    path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view"),
+    path('pdf_download/', views.DownloadPDF.as_view(), name="pdf_download"),
 
 ]
